7afaffe120636ac38f02514439c0089cb57dd1d2
codefotech <seamuddin2020@gmail.com>
2023-04-11 06:17:19 +0600

    update

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index, name='index'),
+    path('add_appointment/', add_appointment, name='appointment-add'),
+    path('appointment/', appintment, name='appointment'),
 ]
